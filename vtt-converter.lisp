4976d9627c235306a8173d55b3e9d68cb524ab3a
Sebastián Monía <code@sebasmonia.com>
2022-05-17 21:22:44 -0600

    Rename converters -> vtt-converter

--- vtt-converter.lisp
+++ vtt-converter.lisp
@@ -5,10 +5,24 @@
 
 ;; This will make more sense when (if?) I convert the cons cells and p-text list
 ;; to proper classes, in that case the conversion will more tidy.
-(defgeneric to-vtt-string (entity)
+(defgeneric to-vtt-format (entity)
   (:documentation "Generate a VTT string out of a given TTML ENTITY."))
 
-(defmethod to-vtt-string ((entity paragraph))
+(defmethod to-vtt-format ((entity ttml-subs-file))
+  "Return a VTT file from a `paragraph' ENTITY."
+  (format nil "~a --> ~a align:start line:~a position:~a~%~a"
+          ;; TODO: reminder, the time conversions need adjustment
+          (car (begin-end entity))
+          (cdr (begin-end entity))
+          ;; TODO: which TTML property maps to what in VTT depends on the
+          ;; writingMode of the source file. I'm assuming lrtd (the default)
+          ;; line is the x coordinate of the region's origin
+          (car (origin (region entity)))
+          ;; position is the y coordinate of the region's origin
+          (cdr (origin (region entity)))
+          (p-text-to-cue (p-texts entity))))
+
+(defmethod to-vtt-format ((entity paragraph))
   "Return a VTT cue (as string) from a `paragraph' ENTITY."
   (format nil "~a --> ~a align:start line:~a position:~a~%~a"
           ;; TODO: reminder, the time conversions need adjustment
